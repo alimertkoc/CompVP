6638956d4b181bab20cc6f30423cb34002215d43
Mert Koc <mert.koc@fau.de>
2024-11-10 16:51:46 +0100

    get STL10 from torchvision.datasets

--- Part1/code/main.py
+++ Part1/code/main.py
@@ -1,27 +1,18 @@
-import shutil
-import os
-import stl10
-
-if not os.path.exists('./data/img'):
-    stl10.main()
-
-if os.path.exists('./img'):
-    # Download latest version
-    shutil.move('./img', './data')
-
 from VisualAcuityDataset import VisualAcuityDataset
 from LimitedColorPerception import LimitedColorPerceptionDataset
 import numpy as np
 import matplotlib.pyplot as plt
 from torchvision import transforms
+from torchvision.datasets import STL10
 
+dataset = STL10('./data', split='train', download=True)
 # Create the VisualAcuityDataset with a specific month_age
 month_ages = list(range(0, 13))  # Create a list of ages from 0 to 12
 va_datasets = []
 lc_datasets=[]
 for month_age in month_ages:
-    va_datasets.append(VisualAcuityDataset(train_dataset, month_age=month_age))
-    lc_datasets.append(LimitedColorPerceptionDataset(train_dataset  , month_age=month_age))
+    va_datasets.append(VisualAcuityDataset(dataset, month_age=month_age))
+    lc_datasets.append(LimitedColorPerceptionDataset(dataset, month_age=month_age))
 
 
 # Now you can use va_dataset with a DataLoader
@@ -42,7 +33,7 @@ def show_images_in_sequence(data_loaders):
     plt.figure(figsize=(25,25))
     for month_age,data_loader in zip(month_ages,data_loaders):
         for images,_ in data_loader:
-            first_image=images[0].permute(1, 2, 0).numpy()*255
+            first_image=images[15].permute(1, 2, 0).numpy()*255
             first_image=first_image.astype(np.uint8)
             
             plt.subplot(1,len(data_loaders),month_age+1)
