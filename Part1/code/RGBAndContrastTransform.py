cab5cfcf0dc034012dcaed6f70eca1551fa47b47
Mert Koc <mert.koc@fau.de>
2024-11-20 11:38:37 +0100

    fixed docstring

--- Part1/code/RGBAndContrastTransform.py
+++ Part1/code/RGBAndContrastTransform.py
@@ -13,7 +13,6 @@ class RGBAndContrastTransform:
         Transforms color property values (color channels and contrast) of an image tensor.
 
         Args:
-        - min_value (float): The minimum value to clamp the color property to.
         - max_value (float): The maximum value to clamp the color property to.
         - channel (int or None): The channel index to apply clamping to. (`None` for all channels)
         - contrast_factor (float): The factor to adjust the contrast of the image.
